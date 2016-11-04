152a1cd7ac1ad246cf711b41ecfd4e10f1114ce7
Kevin Novak <kevinmnovak@yahoo.com>
2016-11-03 21:23:50 -0400

    Remembers inventory on leave and return

--- src/me/kevinnovak/infiniteinventory/CustomInventory.java
+++ src/me/kevinnovak/infiniteinventory/CustomInventory.java
@@ -2,6 +2,7 @@ package me.kevinnovak.infiniteinventory;
 
 import java.util.HashMap;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -13,6 +14,11 @@ public class CustomInventory {
 	private HashMap<Integer, HashMap<Integer, ItemStack>> items = new HashMap<Integer, HashMap<Integer, ItemStack>>();;
 	
 	CustomInventory(Player player) {
+		this.player = player;
+		this.savePage();
+	}
+	
+	void setPlayer(Player player) {
 		this.player = player;
 	}
 	
@@ -33,17 +39,22 @@ public class CustomInventory {
 	}
 	
 	Boolean pageExists(Integer page) {
-		HashMap<Integer, ItemStack> pageItems = this.items.get(page);
-		if (pageItems != null) {
+		if (items.containsKey(page)) {
 		    return true;
 		}
 		return false;
 	}
 	
 	void showPage() {
+		this.showPage(this.page);
+	}
+	
+	void showPage(Integer page) {
+		this.page = page;
 		for(int i=0; i<27; i++) {
 			this.player.getInventory().setItem(i+9, items.get(this.page).get(i));
 		}
+		this.player.sendMessage("After Show - Page: " + this.page);
 	}
 	
 	void nextPage() {
@@ -53,14 +64,20 @@ public class CustomInventory {
 			createPage(this.page);
 		}
 		this.showPage();
+		this.savePage();
+		this.player.sendMessage("After Next - Page: " + this.page);
+		Bukkit.getLogger().info("Player: " + player.getName());
+		Bukkit.getLogger().info("Page: " + this.page);
 	}
 	
 	void prevPage() {
-		this.savePage();
 		if (this.page > 0) {
+			this.savePage();
 			this.page = this.page - 1;
+			this.showPage();
+			this.savePage();
 		}
-		this.showPage();
+		this.player.sendMessage("After Prev - Page: " + this.page);
 	}
 	
 	
