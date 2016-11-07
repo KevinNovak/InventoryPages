fc25276407865cb648ecb51d7f4cd5c089c92f4b
Kevin Novak <kevinmnovak@yahoo.com>
2016-11-07 14:22:04 -0500

    Cleanup

--- src/me/kevinnovak/inventorypages/InventoryPages.java
+++ src/me/kevinnovak/inventorypages/InventoryPages.java
@@ -29,21 +29,21 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class InventoryPages extends JavaPlugin implements Listener{
-	private HashMap<String, CustomInventory> playerInvs = new HashMap<String, CustomInventory>();
+public class InventoryPages extends JavaPlugin implements Listener {
+    private HashMap < String, CustomInventory > playerInvs = new HashMap < String, CustomInventory > ();
     ColorConverter colorConv = new ColorConverter(this);
-	
-	private ItemStack nextItem, prevItem, noActionItem;
-	private Integer prevPos, nextPos;
+
+    private ItemStack nextItem, prevItem, noActionItem;
+    private Integer prevPos, nextPos;
 
     // ======================
     // Enable
     // ======================
     public void onEnable() {
         saveDefaultConfig();
-        
+
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
-        
+
         if (getConfig().getBoolean("metrics")) {
             try {
                 MetricsLite metrics = new MetricsLite(this);
@@ -55,278 +55,279 @@ public class InventoryPages extends JavaPlugin implements Listener{
         } else {
             Bukkit.getServer().getLogger().info("[InventoryPages] Metrics Disabled.");
         }
-        
+
         // initialize next, prev items
         initItems();
-        
+
         // load all online players into hashmap
-        for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-        	try {
-        		loadInvFromFileIntoHashMap(player);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+        for (Player player: Bukkit.getServer().getOnlinePlayers()) {
+            try {
+                loadInvFromFileIntoHashMap(player);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        
+
         Bukkit.getServer().getLogger().info("[InventoryPages] Plugin Enabled!");
     }
 
-	// ======================
+    // ======================
     // Disable
     // ======================
     public void onDisable() {
-        for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-        	// update inventories to hashmap and save to file
-        	updateInvToHashMap(player);
-			saveInvFromHashMapToFile(player);
+        for (Player player: Bukkit.getServer().getOnlinePlayers()) {
+            // update inventories to hashmap and save to file
+            updateInvToHashMap(player);
+            saveInvFromHashMapToFile(player);
         }
         Bukkit.getServer().getLogger().info("[InventoryPages] Plugin Disabled!");
     }
-    
-	// ======================
+
+    // ======================
     // Initialize Next Item
     // ======================
-    @SuppressWarnings("deprecation")
-	public void initItems() {
-    	prevItem = new ItemStack(getConfig().getInt("items.prev.ID"), 1, (short) getConfig().getInt("items.prev.variation"));
+    @
+    SuppressWarnings("deprecation")
+    public void initItems() {
+        prevItem = new ItemStack(getConfig().getInt("items.prev.ID"), 1, (short) getConfig().getInt("items.prev.variation"));
         ItemMeta prevItemMeta = prevItem.getItemMeta();
         prevItemMeta.setDisplayName(colorConv.convertConfig("items.prev.name"));
         prevItemMeta.setLore(colorConv.convertConfigList("items.prev.lore"));
         prevItem.setItemMeta(prevItemMeta);
-        
+
         prevPos = getConfig().getInt("items.prev.position");
-    	
+
         nextItem = new ItemStack(getConfig().getInt("items.next.ID"), 1, (short) getConfig().getInt("items.next.variation"));
         ItemMeta nextItemMeta = nextItem.getItemMeta();
         nextItemMeta.setDisplayName(colorConv.convertConfig("items.next.name"));
         nextItemMeta.setLore(colorConv.convertConfigList("items.next.lore"));
         nextItem.setItemMeta(nextItemMeta);
-        
+
         nextPos = getConfig().getInt("items.next.position");
-        
+
         noActionItem = new ItemStack(getConfig().getInt("items.noAction.ID"), 1, (short) getConfig().getInt("items.noAction.variation"));
         ItemMeta noActionItemMeta = noActionItem.getItemMeta();
         noActionItemMeta.setDisplayName(colorConv.convertConfig("items.noAction.name"));
         noActionItemMeta.setLore(colorConv.convertConfigList("items.noAction.lore"));
         noActionItem.setItemMeta(noActionItemMeta);
     }
-    
-	// =========================
+
+    // =========================
     // Save Inventory From HashMap To File
     // =========================
-	public void saveInvFromHashMapToFile(Player player) {
-		String playerUUID = player.getUniqueId().toString();
-		if (playerInvs.containsKey(playerUUID)) {
-			File playerFile = new File (getDataFolder() + "/inventories/" + playerUUID + ".yml");
-			FileConfiguration playerData = YamlConfiguration.loadConfiguration(playerFile);
-			
-	    	for(Entry<Integer, ArrayList<ItemStack>> pageItemEntry : playerInvs.get(playerUUID).getItems().entrySet()) {
-	    		for(int i = 0; i < pageItemEntry.getValue().size(); i++) {
-	    			playerData.set(playerUUID + "." + pageItemEntry.getKey() + "." + i, InventoryStringDeSerializer.toBase64(pageItemEntry.getValue().get(i)));
-	    		}
-	    	}
-	    	
-	    	try {
-				playerData.save(playerFile);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	// =========================
+    public void saveInvFromHashMapToFile(Player player) {
+        String playerUUID = player.getUniqueId().toString();
+        if (playerInvs.containsKey(playerUUID)) {
+            File playerFile = new File(getDataFolder() + "/inventories/" + playerUUID + ".yml");
+            FileConfiguration playerData = YamlConfiguration.loadConfiguration(playerFile);
+
+            for (Entry < Integer, ArrayList < ItemStack >> pageItemEntry: playerInvs.get(playerUUID).getItems().entrySet()) {
+                for (int i = 0; i < pageItemEntry.getValue().size(); i++) {
+                    playerData.set(playerUUID + "." + pageItemEntry.getKey() + "." + i, InventoryStringDeSerializer.toBase64(pageItemEntry.getValue().get(i)));
+                }
+            }
+
+            try {
+                playerData.save(playerFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // =========================
     // Load Inventory From File Into HashMap
     // =========================
-	public void loadInvFromFileIntoHashMap(Player player) throws IOException {
-    	int maxPage = 1;
-		for (int i = 2; i < 101; i ++) {
-			if (player.hasPermission("inventorypages.pages." + i)) {
-				maxPage = i - 1;
-			}
-		}
-    	
-		String playerUUID = player.getUniqueId().toString();
-    	CustomInventory inventory = new CustomInventory(player, maxPage, prevItem, prevPos, nextItem, nextPos, noActionItem);
-    
-		File playerFile = new File (getDataFolder() + "/inventories/" + playerUUID + ".yml");
-		FileConfiguration playerData = YamlConfiguration.loadConfiguration(playerFile);
-		
-    	if(playerFile.exists() && playerData.contains(playerUUID)) {
-    		HashMap<Integer, ArrayList<ItemStack>> pageItemHashMap = new HashMap<Integer, ArrayList<ItemStack>>();
+    public void loadInvFromFileIntoHashMap(Player player) throws IOException {
+        int maxPage = 1;
+        for (int i = 2; i < 101; i++) {
+            if (player.hasPermission("inventorypages.pages." + i)) {
+                maxPage = i - 1;
+            }
+        }
 
-        	int pageNum = 0;
-        	Boolean pageExists = playerData.contains(playerUUID + "." + pageNum);
+        String playerUUID = player.getUniqueId().toString();
+        CustomInventory inventory = new CustomInventory(player, maxPage, prevItem, prevPos, nextItem, nextPos, noActionItem);
 
-        	Bukkit.getLogger().info("Starting Loop + Page Exists: " + pageExists);
-        	while (pageExists) {
-        		Bukkit.getLogger().info("Loading " + playerUUID + "'s Page: " + pageNum);
-        		ArrayList<ItemStack> pageItems = new ArrayList<ItemStack>(25);
-        		for(int i = 0; i < 25; i++) {
-        			ItemStack item = InventoryStringDeSerializer.stacksFromBase64(playerData.getString(playerUUID + "." + pageNum + "." + i))[0];
-        			pageItems.add(item);
-        		}
-        		pageItemHashMap.put(pageNum, pageItems);
-        		
-        		pageNum++;
-        		pageExists = playerData.contains(playerUUID + "." + pageNum);
-        	}
-        	inventory.setItems(pageItemHashMap);
+        File playerFile = new File(getDataFolder() + "/inventories/" + playerUUID + ".yml");
+        FileConfiguration playerData = YamlConfiguration.loadConfiguration(playerFile);
 
-    	}
-    	playerInvs.put(playerUUID, inventory);
-    	playerInvs.get(playerUUID).showPage(0);
-	}
-	
-	// =========================
+        if (playerFile.exists() && playerData.contains(playerUUID)) {
+            HashMap < Integer, ArrayList < ItemStack >> pageItemHashMap = new HashMap < Integer, ArrayList < ItemStack >> ();
+
+            int pageNum = 0;
+            Boolean pageExists = playerData.contains(playerUUID + "." + pageNum);
+
+            Bukkit.getLogger().info("Starting Loop + Page Exists: " + pageExists);
+            while (pageExists) {
+                Bukkit.getLogger().info("Loading " + playerUUID + "'s Page: " + pageNum);
+                ArrayList < ItemStack > pageItems = new ArrayList < ItemStack > (25);
+                for (int i = 0; i < 25; i++) {
+                    ItemStack item = InventoryStringDeSerializer.stacksFromBase64(playerData.getString(playerUUID + "." + pageNum + "." + i))[0];
+                    pageItems.add(item);
+                }
+                pageItemHashMap.put(pageNum, pageItems);
+
+                pageNum++;
+                pageExists = playerData.contains(playerUUID + "." + pageNum);
+            }
+            inventory.setItems(pageItemHashMap);
+
+        }
+        playerInvs.put(playerUUID, inventory);
+        playerInvs.get(playerUUID).showPage(0);
+    }
+
+    // =========================
     // Update Inventory To HashMap
     // =========================
-	public void updateInvToHashMap(Player player) {
-		String playerUUID = player.getUniqueId().toString();
-		if(playerInvs.containsKey(playerUUID)) {
-			playerInvs.get(playerUUID).saveCurrentPage();
-		}
-	}
-	
-	// =========================
+    public void updateInvToHashMap(Player player) {
+        String playerUUID = player.getUniqueId().toString();
+        if (playerInvs.containsKey(playerUUID)) {
+            playerInvs.get(playerUUID).saveCurrentPage();
+        }
+    }
+
+    // =========================
     // Remove Inventory From HashMap
     // =========================
-	public void removeInvFromHashMap(Player player) {
-		String playerUUID = player.getUniqueId().toString();
-		if(playerInvs.containsKey(playerUUID)) {
-			playerInvs.remove(playerUUID);
-		}
-	}
+    public void removeInvFromHashMap(Player player) {
+        String playerUUID = player.getUniqueId().toString();
+        if (playerInvs.containsKey(playerUUID)) {
+            playerInvs.remove(playerUUID);
+        }
+    }
 
-	// =========================
+    // =========================
     // Login
     // =========================
     @EventHandler
     public void playerJoin(PlayerJoinEvent event) throws InterruptedException, IOException {
-    	Player player = event.getPlayer();
-		loadInvFromFileIntoHashMap(player);
+        Player player = event.getPlayer();
+        loadInvFromFileIntoHashMap(player);
     }
-    
+
     // =========================
     // Logout
     // =========================
     @EventHandler
     public void playerQuit(PlayerQuitEvent event) throws InterruptedException {
-    	Player player = event.getPlayer();
-    	updateInvToHashMap(player);
-    	saveInvFromHashMapToFile(player);
-    	removeInvFromHashMap(player);
+        Player player = event.getPlayer();
+        updateInvToHashMap(player);
+        saveInvFromHashMapToFile(player);
+        removeInvFromHashMap(player);
     }
-    
+
     // =========================
     // Death
     // =========================
     @EventHandler
     public void onDeath(PlayerDeathEvent event) {
-    	Player player = event.getEntity();
-    	
-    	//save items before death
-    	updateInvToHashMap(player);
-    	
-    	List<ItemStack> drops = event.getDrops();
+        Player player = event.getEntity();
+
+        //save items before death
+        updateInvToHashMap(player);
+
+        List < ItemStack > drops = event.getDrops();
         event.setKeepLevel(true);
-        ListIterator<ItemStack> litr = drops.listIterator();
-        while(litr.hasNext()){
-        	ItemStack stack = litr.next();
-	        if (stack.getType() == prevItem.getType() && stack.getItemMeta().getDisplayName() == prevItem.getItemMeta().getDisplayName()) {
-	            litr.remove();
-	        } else if (stack.getType() == nextItem.getType() && stack.getItemMeta().getDisplayName() == nextItem.getItemMeta().getDisplayName()) {
-	        	litr.remove();
-	        } else if (stack.getType() == noActionItem.getType() && stack.getItemMeta().getDisplayName() == noActionItem.getItemMeta().getDisplayName()) {
-	        	litr.remove();
-	        }
+        ListIterator < ItemStack > litr = drops.listIterator();
+        while (litr.hasNext()) {
+            ItemStack stack = litr.next();
+            if (stack.getType() == prevItem.getType() && stack.getItemMeta().getDisplayName() == prevItem.getItemMeta().getDisplayName()) {
+                litr.remove();
+            } else if (stack.getType() == nextItem.getType() && stack.getItemMeta().getDisplayName() == nextItem.getItemMeta().getDisplayName()) {
+                litr.remove();
+            } else if (stack.getType() == noActionItem.getType() && stack.getItemMeta().getDisplayName() == noActionItem.getItemMeta().getDisplayName()) {
+                litr.remove();
+            }
         }
     }
-    
+
     // =========================
     // Respawn
     // =========================
     @EventHandler
     public void onRespawn(PlayerRespawnEvent event) {
-    	Player player = event.getPlayer();
-    	String playerUUID = player.getUniqueId().toString();
-    	
-    	// saves empty inventory (other than next and prev)
-    	// disable this if you want to keep items
-    	updateInvToHashMap(player);
-    	
-    	playerInvs.get(playerUUID).showPage();
+        Player player = event.getPlayer();
+        String playerUUID = player.getUniqueId().toString();
+
+        // saves empty inventory (other than next and prev)
+        // disable this if you want to keep items
+        updateInvToHashMap(player);
+
+        playerInvs.get(playerUUID).showPage();
     }
-    
+
     // =========================
     // Inventory Click
     // =========================
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-    	Inventory eventInv = event.getClickedInventory();
-    	if (eventInv != null) {
-        	InventoryType eventInvType = event.getClickedInventory().getType();
-        	if (eventInvType != null) {
-        		if (eventInvType == InventoryType.PLAYER) {
-        	    	HumanEntity human = event.getWhoClicked();
-        			if (human instanceof Player) {
-        				Player player = (Player) human;
-        				GameMode gm = player.getGameMode();
-        				if(gm != GameMode.CREATIVE) {
-        					String playerUUID = (String) player.getUniqueId().toString();
-        					int slot = event.getSlot();
-        					player.sendMessage("Clicked Slot: " + slot);
-        			    	if (slot == prevPos+9) {
-        			    		event.setCancelled(true);
-        			    		playerInvs.get(playerUUID).prevPage();
-        			    	} else if (slot == nextPos+9) {
-        			    		event.setCancelled(true);
-        			    		playerInvs.get(playerUUID).nextPage();
-        			    	}
-        				}
-        			}
-        		}
-        	}
-    	}
+        Inventory eventInv = event.getClickedInventory();
+        if (eventInv != null) {
+            InventoryType eventInvType = event.getClickedInventory().getType();
+            if (eventInvType != null) {
+                if (eventInvType == InventoryType.PLAYER) {
+                    HumanEntity human = event.getWhoClicked();
+                    if (human instanceof Player) {
+                        Player player = (Player) human;
+                        GameMode gm = player.getGameMode();
+                        if (gm != GameMode.CREATIVE) {
+                            String playerUUID = (String) player.getUniqueId().toString();
+                            int slot = event.getSlot();
+                            player.sendMessage("Clicked Slot: " + slot);
+                            if (slot == prevPos + 9) {
+                                event.setCancelled(true);
+                                playerInvs.get(playerUUID).prevPage();
+                            } else if (slot == nextPos + 9) {
+                                event.setCancelled(true);
+                                playerInvs.get(playerUUID).nextPage();
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
-    
+
     // =========================
     // Inventory Pickup
     // =========================
     @EventHandler
     public void onInventoryPickupItem(InventoryPickupItemEvent event) {
-		ItemStack item = event.getItem().getItemStack();
+        ItemStack item = event.getItem().getItemStack();
         if (item.getType() == prevItem.getType() && item.getItemMeta().getDisplayName() == prevItem.getItemMeta().getDisplayName()) {
             event.setCancelled(true);
         } else if (item.getType() == nextItem.getType() && item.getItemMeta().getDisplayName() == nextItem.getItemMeta().getDisplayName()) {
-        	event.setCancelled(true);
+            event.setCancelled(true);
         } else if (item.getType() == noActionItem.getType() && item.getItemMeta().getDisplayName() == noActionItem.getItemMeta().getDisplayName()) {
-        	event.setCancelled(true);
+            event.setCancelled(true);
         }
     }
-    
+
     // =========================
     // Item Drop
     // =========================
     @EventHandler
     public void onPlayerDropItem(PlayerDropItemEvent event) {
-		ItemStack item = event.getItemDrop().getItemStack();
+        ItemStack item = event.getItemDrop().getItemStack();
         if (item.getType() == prevItem.getType() && item.getItemMeta().getDisplayName() == prevItem.getItemMeta().getDisplayName()) {
             event.setCancelled(true);
         } else if (item.getType() == nextItem.getType() && item.getItemMeta().getDisplayName() == nextItem.getItemMeta().getDisplayName()) {
-        	event.setCancelled(true);
+            event.setCancelled(true);
         } else if (item.getType() == noActionItem.getType() && item.getItemMeta().getDisplayName() == noActionItem.getItemMeta().getDisplayName()) {
-        	event.setCancelled(true);
+            event.setCancelled(true);
         }
     }
-    
+
     // =========================
     // GameMode Change
     // =========================
     @EventHandler
     public void onPlayerGameModeChange(PlayerGameModeChangeEvent event) {
-    	Player player = event.getPlayer();
+        Player player = event.getPlayer();
         String playerUUID = player.getUniqueId().toString();
         playerInvs.get(playerUUID).saveCurrentPage();
-    	playerInvs.get(playerUUID).showPage(event.getNewGameMode());
+        playerInvs.get(playerUUID).showPage(event.getNewGameMode());
     }
 }
